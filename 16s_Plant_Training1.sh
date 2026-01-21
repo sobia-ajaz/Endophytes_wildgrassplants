25f80e85d5dcac65d8f237922a48e2ddbc574d47
Sobia Ajaz <sobia.ajaz@ucd.ie>
2026-01-21 13:45:46 +0000

    Add files via upload

---
+++ 16s_Plant_Training1.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -l
+#SBATCH --job-name=Training
+# speficity number of nodes 
+#SBATCH -N 5
+
+# specify number of tasks/cores per node required
+#SBATCH --ntasks-per-node 10
+
+# specify the walltime e.g 20 mins
+#SBATCH -t 06:00:00
+
+# set to email at start,end and failed jobs
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sobia.ajaz@ucd.ie
+
+# run from current directory
+cd $SLURM_SUBMIT_DIR
+
+# command to use
+hostname
+
+#activate qiime2
+conda activate qiime2-amplicon-2024.2
+
+#Import the files
+qiime tools import --input-path Training_Dataset --type 'SampleData[PairedEndSequencesWithQuality]' --input-format CasavaOneEightSingleLanePerSampleDirFmt --output-path demux-paired-end_16S-Plant-Training.qza
+
+#To see the imported data
+qiime demux summarize --i-data demux-paired-end_16S-Plant-Training.qza --o-visualization demux-paired-end_16S-Plant-Training.qzv
+
+#RRIMERS (16S) V4
+#Amplicon size: ~250bp
+#Primer forward (515F): (19 bp)GTGCCAGCMGCCGCGGTAA
+#Primer reverse (806R): (20 bp)GGACTACNVGGGTHTCTAAT
+
+#In the following command, --p-front-f is the forward primer (5’-3’),
+#--p-front-r the reverse primer (5’-3’)
+#You can also included --p-adapter-f, which is the reverse complement of the reverse primer, and --p-adapter-r, which is the reverse complement of the forward primer
+#These adapters would be present in the reads if the amplicon region is shorter than the read length - in this case both primers are typically present in each forward and reverse read
+# and you should treat them like “linked adapters”, where the forward primer is linked with the reverse complement of the reverse primer, and vice versa.
+#--p-discard-untrimmed discards any untrimmed reads. In this command, reads will only be discarded if they don’t have either the normal primer or the linked adapter (just having one is ok)
+#--verbose gives you a detailed output of how many reads and base pairs were trimmed off in each sample (by default if this was run as a script on the ccv it would be stored in the slurm file)
+
+#Cutadapt: 
+#https://docs.qiime2.org/2024.2/plugins/available/cutadapt/trim-paired/
+#https://cutadapt.readthedocs.io/en/stable/guide.html#five-prime-adapters
+
+qiime cutadapt trim-paired --i-demultiplexed-sequences demux-paired-end_16S-Plant-Training.qza --p-front-f GTGCCAGCMGCCGCGGTAA --p-front-r GGACTACNVGGGTHTCTAAT --p-discard-untrimmed --o-trimmed-sequences demux-cutadapt_16S-Plant-Training.qza --verbose
+
+#To see the trimmed data
+qiime demux summarize --i-data demux-cutadapt_16S-Plant-Training.qza --o-visualization demux-cutadapt_16S-Plant-Training.qzv
+
+#DADA2
+It’s important to take a look at the quality plots and choose a good place for DADA2 to truncate the reads. –p-trunc-len-f defines the length the forward reads will be trimmed and –p-trunc-len-r defines the length the reverse reads will be trimmed at. You want to trim off low-quality bases because DADA2 has a initial filtering step based on quality, so it will discard the whole read if the quality drops off too much at the end (which could otherwise be retained if truncated appropriately). But you also want to keep long enough reads for DADA2 to be able to merge. The default min overlap is 12 but can be decreased down to 4 with –p-min-overlap
+
+
+#Overlapping region
+#220+180=400-250=150nts
+#220=length of forward read, 180=length of reverse read, 250=amplicon size
+
+
+qiime dada2 denoise-paired --i-demultiplexed-seqs demux-cutadapt_16S-Plant-Training.qza --p-trim-left-f 0 --p-trim-left-r 0 --p-trunc-len-f 220 --p-trunc-len-r 180 --o-table feature-table.qza --o-representative-sequences rep-seqs.qza --o-denoising-stats denoizing_stats.qza
+
+#Visualization
+
+qiime metadata tabulate --m-input-file denoizing_stats.qza --o-visualization denoizing_stats.qzv
+
+qiime feature-table summarize --i-table feature-table.qza --o-visualization feature-table.qzv --m-sample-metadata-file metadata-16S-Training.txt 
+
+qiime feature-table tabulate-seqs --i-data rep-seqs.qza --o-visualization rep-seqs.qzv
+
+qiime metadata tabulate --m-input-file denoizing_stats.qza --o-visualization denoizing_stats.qzv
+
+#Export to tsv for Visualization and removal of singletones
+
+qiime tools export --input-path feature-table.qza --output-path exported-table
+
+cd exported-table
+
+biom convert -i feature-table.biom -o feature-table.tsv --to-tsv
